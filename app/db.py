a1406b39cd80cf603d8499ac4f33f243d069f8dc
shruti8302 <93856491+shruti8302@users.noreply.github.com>
2025-01-15 23:08:30 +0530

    created database connection and created table 'travel_knowledge_base'

--- app/db.py
+++ app/db.py
@@ -1,1 +1,40 @@
-print("Hello world...")
+import sqlite3
+
+
+"""
+Creating connection to DB(travel.db)
+"""
+def connect_db():
+    conn = sqlite3.connect('travel.db')
+    cursor = conn.cursor()
+    return cursor
+
+
+
+"""
+Creating 'travel_knowledge_base_table' 
+"""
+def create_travel_knowledge_base_table():
+    cursor = connect_db()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS travel_knowledge_base (
+            id INTEGER PRIMARY KEY,
+            destination TEXT,
+            description TEXT,
+            details TEXT,
+            metadata TEXT,
+            embeding TEXT
+        )
+    ''')
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    print(" Started ".center(100, "-"))
+    # create_travel_knowledge_base_table()
+    
