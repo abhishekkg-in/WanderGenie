65636b639c5bf2deafcd8059d2da3781aba9020d
shruti8302 <93856491+shruti8302@users.noreply.github.com>
2025-01-18 20:57:26 +0530

    first version of wonder genie is ready for demo

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from data.db import fetch_travel_knowledge_base_table, update_embeding, fetch_embedings
-from embedings.embedings import create_embeding, update_vector_db_with_embeding
+from embedings.embedings import create_embeding, update_vector_db_with_embeding, generate_embeding
 from response_generator.generate_response import generate_response_from_llama
 from data.prompts import prompt_guardrails, prompt_travel_recomendation
-
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
 
 
 """
-Creating embeding out of travel_knowledge_base data 
-and updating table with embedings.
+    This function updates the travel knowledge base with embeddings.
+    It fetches the rows from the travel knowledge base table, creates embeddings
+    for each row using the 'create_embedding' function, and then updates the 
+    respective entries in the knowledge base with the newly created embeddings.
 """
 def update_travel_knowledge_base_with_embedings():
     rows = fetch_travel_knowledge_base_table()
@@ -17,8 +21,82 @@ def update_travel_knowledge_base_with_embedings():
 
 
 
-def chat_with_genie():
+"""
+    This function generates embeddings for the travel knowledge base using
+    the SentenceTransformer model. It fetches the rows from the knowledge base,
+    generates embeddings for the content of each row, and then updates the knowledge
+    base with the generated embeddings. It also prints relevant information for debugging.
+"""
+def generate_embedings():
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    embedings = []
+    rows = fetch_travel_knowledge_base_table()
+    print("rows -> ", rows)
+    for row in rows:
+        embeding = model.encode(row[1]).tolist()
+        print("\n embeding -> ", embeding)
+        print("row_id -> ", row[0])
+        update_embeding(row[0], embeding)
+        embedings.append(embeding)
 
+
+
+"""
+    This function updates the vector database by fetching embeddings from the
+    knowledge base and adding them to a FAISS index for similarity search.
+    It uses the pre-trained SentenceTransformer model to generate embeddings
+    and stores them in the FAISS index, which is optimized for efficient vector search.
+"""
+def update_vector_db():
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    ids, destinations, descriptions, details, metadata, db_embeddings = fetch_embedings()
+    index = faiss.IndexFlatL2(db_embeddings.shape[1])
+    index.add(db_embeddings)
+
+
+
+"""
+    This function searches a vector database for the most similar destinations
+    to the input query. It uses a pre-trained SentenceTransformer model to encode
+    the query into an embedding and compares it with the existing embeddings stored
+    in the FAISS index using Euclidean (L2) distance. The function returns the
+    top k most similar results based on the search.
+"""
+def search_vector_db(query="best place to visit in rome"):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    query_embedding = model.encode(query, convert_to_tensor=False).astype('float32')
+    ids, destinations, descriptions, details, metadata, db_embeddings = fetch_embedings()
+
+    top_k = 3
+    dimension = db_embeddings.shape[1]
+    faiss_index = faiss.IndexFlatL2(dimension)
+    faiss_index.add(db_embeddings)
+    distances, indices = faiss_index.search(np.array([query_embedding]), top_k)
+    results = []
+    for i in range(top_k):
+        idx = indices[0][i]
+        results.append({
+            "id": ids[idx],
+            "destination": destinations[idx],
+            "description": descriptions[idx],
+            "details": details[idx],
+            "metadata": metadata[idx],
+            "distance": distances[0][i]
+        })
+
+    # print("result -->> ", results)
+    return results
+
+
+
+"""
+    This function simulates a chatbot-like interaction, allowing the user to ask
+    for trip details or quit the conversation. It presents a menu for the user to 
+    choose from and responds based on the selected option. If the user asks for trip 
+    details, the query is processed using a model and a vector search to return 
+    relevant travel recommendations.
+"""
+def chat_with_genie():
     flag = True
     print(" Chat window started ".center(100, "-"))
     while flag:
@@ -39,7 +117,8 @@ def chat_with_genie():
                 print("".center(100, "-"))
                 continue
             else:
-                res = generate_response_from_llama(prompt_travel_recomendation(query))
+                context = search_vector_db(query=query)
+                res = generate_response_from_llama(prompt_travel_recomendation(query=query, context=context))
                 print("Genie -> ", res)
                 print()
                 print("".center(100, "-"))
@@ -52,19 +131,21 @@ def chat_with_genie():
 
 
 
-
-
-
-
 """
-===================================================================
 =========================== MAIN FUNCTION =========================
-===================================================================
+This is the main entry point for the program. It initiates the chat window
+where users can interact with the Genie to ask for trip details or quit.
+The other functions for generating embeddings, updating the vector database, 
+and searching the vector database are commented out but can be activated 
+as needed for different functionalities.
 """
 def main():
-    # print(" Main Started ".center(100, '-'))
     print()
+    # generate_embedings()
+    # update_vector_db()
+    # search_vector_db()
     chat_with_genie()
+
 
 
 if __name__=="__main__":
--- data/db.py
+++ data/db.py
@@ -1,8 +1,12 @@
 import sqlite3
+import json
+import numpy as np
+
 
 
 """
-Creating connection to DB(travel.db)
+    This function establishes a connection to the SQLite database and returns a cursor object
+    that can be used to execute SQL queries on the connected database. The database file used is 'travel.db'.
 """
 def connect_db():
     conn = sqlite3.connect('travel.db')
@@ -10,8 +14,12 @@ def connect_db():
     return cursor
 
 
+
 """
-Creating 'travel_knowledge_base_table' 
+    This function creates the 'travel_knowledge_base' table in the SQLite database.
+    It checks if the table already exists, and if not, it creates a new table with
+    the specified schema. The table stores information related to travel destinations, 
+    including metadata and embeddings for future use.
 """
 def create_travel_knowledge_base_table():
     cursor = connect_db()
@@ -22,13 +30,41 @@ def create_travel_knowledge_base_table():
             description TEXT,
             details TEXT,
             metadata TEXT,
-            embeding TEXT
+            embeding BLOB
         )
     ''')
 
 
+
 """
-Fetch travel_knowledge_travel table data
+    This function inserts data into the 'travel_knowledge_base' table in the SQLite database.
+    It iterates over a list of data, where each item is expected to contain details about a travel destination.
+    It inserts the information into the table and handles any exceptions such as duplicates or missing keys.
+"""
+def create_travel_knowledge_base_table(data):
+    cursor = connect_db()
+    for item in data:
+        print('item -> ', item)
+        try:
+            cursor.execute('''
+                INSERT INTO travel_knowledge_base (id, destination, description, details, metadata)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (item['Id'], item['destination'], item['description'], item['details'], item['metadata']))
+        except sqlite3.IntegrityError:
+            print(f"Error: Destination '{item['destination']}' already exists.")
+        except KeyError as e:
+            print(f"Error: Missing key '{e}' in data.")
+        else:
+            print(f"inserted data with id {item.get('Id')}")
+    cursor.connection.commit()
+    cursor.close()
+
+
+
+"""
+    This function retrieves all rows from the 'travel_knowledge_base' table.
+    It combines the 'description', 'details', and 'metadata' columns into one field 
+    and returns the results as a list of tuples. Each tuple contains the id and the concatenated string.
 """
 def fetch_travel_knowledge_base_table():
     cursor = connect_db()
@@ -37,26 +73,60 @@ def fetch_travel_knowledge_base_table():
     return rows
 
 
+
 """
-Update travel_knowledge_travel table with embedings
+    This function updates the 'embedding' column for a specific row in the 'travel_knowledge_base' table.
+    It takes an 'id' and the new 'embedding' (a list or array), serializes the embedding into binary format, 
+    and updates the corresponding row in the table.
 """
 def update_embeding(id, embedding):
     cursor = connect_db()
-    cursor.execute("UPDATE travel_knowledge_base SET embedding = ? WHERE id = ?", (str(embedding), id))
-    
+    serialized_embedding = np.array(embedding, dtype=np.float32).tobytes()
+    cursor.execute("UPDATE travel_knowledge_base SET embeding = ? WHERE id = ?", (serialized_embedding, id))
+    cursor.connection.commit()
+
+
 
 """
-Fetch embedings of travel_knowledge_travel table
+    This function fetches all relevant data from the 'travel_knowledge_base' table,
+    including the 'embedding' column which contains serialized embeddings in binary format.
+    It extracts the information into separate lists for easy use and returns the processed data.
 """
 def fetch_embedings():
     cursor = connect_db()
-    cursor.execute("SELECT id, embedding FROM travel_knowledge_base")
+    cursor.execute("SELECT id, destination, description, details, metadata, embeding, typeof(embeding) FROM travel_knowledge_base")
     rows = cursor.fetchall()
-    return rows
+    ids, destinations, descriptions, details, metadata, embeddings = [], [], [], [], [], []
+    for row in rows:
+        ids.append(row[0])
+        destinations.append(row[1])
+        descriptions.append(row[2])
+        details.append(row[3])
+        metadata.append(row[4])
+        embedding_data = row[5]
+        # print("type embeding -->> ", row[6])
+        if isinstance(embedding_data, str):
+            # embedding_data = json.loads(embedding_data)
+            embedding_data = bytes.fromhex(embedding_data)
+        embeddings.append(np.frombuffer(embedding_data, dtype=np.float32))
+    return ids, destinations, descriptions, details, metadata, np.vstack(embeddings)
+
+
+
+"""
+    This is the main function of the script. It loads data from a JSON file and creates or updates 
+    the travel knowledge base table in the SQLite database with the contents of the loaded data.
+"""
+def main():
+    with open('E:\Project\WanderGenie\data\knowledge_base.json', 'r') as f:
+        data = json.loads(f.read())
+        create_travel_knowledge_base_table(data)
 
 
 
 if __name__=="__main__":
     print(" Started ".center(100, "-"))
-    create_travel_knowledge_base_table()
+    main()
+    
+
 
